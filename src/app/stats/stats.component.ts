f87eb589fed1300f255d289ccc4472761edb6f93
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2023-11-10 18:53:55 +0100

    Add slothy boi

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -7,25 +7,34 @@ import {Component} from '@angular/core';
 })
 export class StatsComponent {
     fadeInStats() {
-        this.fade(true);
+        this.fade(true, "sloth");
+        this.fade(true, "github-stats");
+        setTimeout(() => {
+            this.link(true);
+        }, 2000);
+
     }
 
     fadeOutStats() {
-        this.fade(false);
+        this.fade(false, "sloth");
+        this.fade(false, "github-stats");
+        this.link(false);
     }
 
-    private fade(fadeIn: boolean) {
-        let stats = document.getElementById('github-stats');
+    private fade(fadeIn: boolean, elementId: string) {
+        let stats = document.getElementById(elementId);
         if (stats != null) {
             let currentOpacity = window.getComputedStyle(stats, null).getPropertyValue('opacity');
             stats.classList.remove(fadeIn ? 'fade-out' : 'fade-in')
             stats.style.opacity = currentOpacity;
             stats.classList.add(fadeIn ? 'fade-in' : 'fade-out');
         }
+    }
 
+    private link(add: boolean) {
         let icon = document.getElementById('github-icon');
-        if (icon != null) {
-            if (fadeIn) {
+        if (icon != null ) {
+            if (add) {
                 icon.setAttribute('href', 'https://github.com/strangelookingnerd');
                 icon.setAttribute('target', '_blank');
             } else {
