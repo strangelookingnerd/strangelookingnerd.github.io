e160c0ff88eaf3f0cd602677fbb4840d6355b820
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2026-06-28 15:44:05 +0200

    Fix build

--- src/app/particles/particles.component.ts
+++ src/app/particles/particles.component.ts
@@ -1,7 +1,6 @@
 import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 import { NgParticlesService, NgxParticlesModule } from "@tsparticles/angular";
-import { Component } from "@angular/core";
-import { Container, Engine } from "@tsparticles/engine";
+import { Component, OnInit} from "@angular/core";
 
 @Component({
   selector: "app-particles",
@@ -9,19 +8,14 @@ import { Container, Engine } from "@tsparticles/engine";
   imports: [NgxParticlesModule],
   styleUrls: ["./particles.component.css"],
 })
-export class ParticlesComponent {
+export class ParticlesComponent implements OnInit {
   particlesUrl = "./assets/configs/particles.json";
 
   constructor(private readonly ngParticlesService: NgParticlesService) {}
 
   ngOnInit(): void {
     this.ngParticlesService.init(async (engine) => {
-      console.log(engine);
       await loadSlim(engine);
     });
-  }
-
-  particlesLoaded(container: Container): void {
-    console.log(container);
   }
 }
